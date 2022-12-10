17332993e7f27cdb4f8d3c7fc295078d5c696bc7
Mateoswiatek <mateoswiatek@student.agh.edu.pl>
2022-12-10 16:57:41 +0100

    wyszukiwanie_ocena

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ struct student{
     struct student *next_adress;
 };
 
-// dodanie czegos studenic
 //ok
 void wpisz_dane_studenta(struct student *s){
     printf("podaj ocene: \n");
@@ -24,21 +23,22 @@ void wpisz_dane_studenta(struct student *s){
 void wypisz_studenta( struct student s){
     printf("Ocena: %d Nazwisko: %s\n", s.ocena, s.nazwisko);
 }
-
+//ok
 void wypisz_wszystkich(struct student *pierwszy){
     struct student *lista_studentow;
     lista_studentow=pierwszy;
     wypisz_studenta(*pierwszy);
     while(lista_studentow->next_adress){
-        wypisz_studenta(*lista_studentow);
         lista_studentow=lista_studentow->next_adress;
+        wypisz_studenta(*lista_studentow);
     }
 }
-
-void dodaj_studenta(int ilosc, struct student *root){
+//ok
+void dodaj_studentow(int ilosc, struct student *root){
     struct student *wsk_listy;
     int i=0;
     wsk_listy=root; // wsk_listy wskazuje na roota
+    if(!ilosc) {printf("wpisz liczbe >0\n"); return;}
 
     if (czy_pierwszy_raz) { // za pierwszym razem mamy roota, nie ma w nim wartosci
         wpisz_dane_studenta(wsk_listy);
@@ -56,11 +56,34 @@ void dodaj_studenta(int ilosc, struct student *root){
         if(wsk_listy==NULL){printf("blad pamieci"); return;}
         wpisz_dane_studenta(wsk_listy);
     }
+}
 
+void wyszukiwanie(struct student *root, bool rodzaj, int szukana_ocena, char *szukane_nazwisko){
+    struct student *nast_student;
+    nast_student=root;
+
+    if(rodzaj) { //1 - nazwisko 0- ocena
+        printf("Naziwsko to %s\n", szukane_nazwisko);
+        printf("Studenci o nazwisku %s:\n", szukane_nazwisko);
+        if (nast_student->nazwisko == szukane_nazwisko) wypisz_studenta(*nast_student);
+        while (nast_student->next_adress) {
+            nast_student = nast_student->next_adress;
+            if (nast_student->nazwisko == szukane_nazwisko) wypisz_studenta(*nast_student);
+        }
+    }
+    else{
+        printf("Studenci majacy ocene %d:\n", szukana_ocena);
+        if (nast_student->ocena == szukana_ocena) wypisz_studenta(*nast_student);
+        while (nast_student->next_adress) {
+            nast_student = nast_student->next_adress;
+            if (nast_student->ocena == szukana_ocena) wypisz_studenta(*nast_student);
+        }
+    }
 }
 
 
 int main() {
+    int wybor=0;
     struct student *root = 0, **ws_root; // to są unsigned int (u) /  unsigned long int  (lu)
     root = malloc(sizeof(struct student));
     if(root==NULL){printf("blad pamieci"); return 0;}
@@ -72,14 +95,19 @@ int main() {
     printf("ocena roota to %d\n", root->ocena);
     printf("a ws_root ma w sobie: %u\n", *ws_root);
 
+/*
+    printf("dzialania:\n0-dodawanie stuentow\n1-wyswietlanie studentow\n2-wyszukiwanie\n3-usuwanie\n%d-Wyjscie", WYJSCIE);
+    scanf("%d", &wybor); */
 
-
-
-    wpisz_dane_studenta(root);
-    wypisz_studenta(*root);
+    dodaj_studentow(3, root);
 
     wypisz_wszystkich(root);
 
+    char S_nazwisko[100];
+    /*printf("podaj nazwisko: ");
+    scanf("%s", &S_nazwisko); */
+
+    wyszukiwanie(root, 0, 1, S_nazwisko);
 
 
     /*
